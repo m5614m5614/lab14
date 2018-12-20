f990291b7dba772160ff773b4b9c6f82766a2e56
m5614m5614 <m5614m5614ss@gmail.com>
2018-12-20 15:23:48 +0800

    fir

---
+++ app/src/main/java/com/example/user/lab14/Data.java
@@ -0,0 +1,12 @@
+package com.example.user.lab14;
+
+class Data{
+    Result result;
+    class Result{
+        Results[] results;
+        class Results{
+            String Station;
+            String Destination;
+        }
+    }
+}
